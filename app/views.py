b1315ad5760517b68cc0e6dc2b76548c46daa419
Wendy Munyasi <wendymunyasi@gmail.com>
2020-02-01 12:49:42 +0300

    Add homepage route and news route to views.py.

--- app/views.py
+++ app/views.py
@@ -4,8 +4,18 @@ from app import app
 # Views
 @app.route('/')
 def index():
-    
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - Welcome To The best News Website Of The Century'
+    message = 'Boom, done'
+    return render_template('index.html', message=message, title = title)
+
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html', news_id=news_id)
