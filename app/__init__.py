119f12d97f07b9b010b7c98ac5d395f7e1765cf9
Wendy Munyasi <wendymunyasi@gmail.com>
2020-02-01 12:48:03 +0300

    Import DevConfig from .config, add it to the Flask app instance.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 # Initializing the application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
-from app import Views
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from app import views
