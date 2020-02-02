fdc71ba347f6956ed22d7ad07606fbcd228f7b81
Wendy Munyasi <wendymunyasi@gmail.com>
2020-02-02 09:36:09 +0300

    Import get_sources, create variables for each of the sources types using the function, render them to index.html.

--- app/views.py
+++ app/views.py
@@ -1,21 +1,33 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 # Views
 @app.route('/')
 def index():
+
     '''
     View root page function that returns the index page and its data
     '''
+    
+    general_source = get_sources('general')
+    # print(general_source)
+    business_source = get_sources('general')
+    technology_source = get_sources('technology')
+    sports_source = get_sources('sports')
+    entertainment_source = get_sources('entertainment')
+    health_source = get_sources('health')
+    science_source = get_sources('science')
+    
+    
+    title = 'Home - Welcome to the best News website of this century'
+    return render_template('index.html', title = title, general = general_source, business = business_source, technology = technology_source, sports = sports_source, entertainment = entertainment_source, health = health_source, science = science_source)
 
-    title = 'Home - Welcome To The best News Website Of The Century'
-    message = 'Boom, done'
-    return render_template('index.html', message=message, title = title)
+@app.route('/sources/<source_id>')
+def source(source_id):
 
-
-@app.route('/news/<news_id>')
-def news(news_id):
     '''
-    View movie page function that returns the movie details page and its data
+    View source page function that returns the source details page and its data
     '''
-    return render_template('news.html', news_id=news_id)
+    return render_template('source.html',source_id = source_id)
+
