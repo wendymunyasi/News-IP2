5ff4ba4ac35e4413641c4e7f7bb3b34acd36a53b
Wendy Munyasi <wendymunyasi@gmail.com>
2020-02-02 09:30:31 +0300

    Change the api base url in config.py.

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
 
 
 class ProdConfig(Config):
