54e99b8be2086fbea02edfbfa5e26a1a5a4b429b
Wendy Munyasi <wendymunyasi@gmail.com>
2020-02-03 05:13:47 +0300

    Add article base url in config.py.

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
 
 
 class ProdConfig(Config):
