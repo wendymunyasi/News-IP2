00f9fe977ccba621b3686b27edbe8f3cc9de5321
Wendy Munyasi <wendymunyasi@gmail.com>
2020-02-04 11:52:14 +0300

    Change the articles base url to another one.

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?country=us&category={}&apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?country=us&language=en&category={}&apiKey={}'
     
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?language=en&sources={}&apiKey={}'
 
 
 class ProdConfig(Config):
